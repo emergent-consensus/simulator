9adf3eab7c8afa45f90dbf1a3e2d215c8b6f6c2b
Emergent Consensus Simulator <emergentconsensussimulator@gmail.com>
2016-12-04 21:44:42 -0600

    Adding basic routes

--- python.py
+++ python.py
@@ -1,9 +1,23 @@
 from flask import Flask
 app = Flask(__name__)
+from flask_bootstrap import Bootstrap
+from flask import render_template
+
+
+app = Flask(__name__)
+Bootstrap(app)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/network")
+def network_page():
+    return render_template('network.html')
+
+@app.route("/my-rigs")
+def myrig_page():
+    return render_template('my_rigs.html')
 
 if __name__ == "__main__":
     app.run()
