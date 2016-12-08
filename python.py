00183dd2acf9d5ca85cf39a74d042ee2314c31a2
Emergent Consensus Simulator <emergentconsensussimulator@gmail.com>
2016-12-07 20:55:48 -0600

    Adding panel for rig details.

--- python.py
+++ python.py
@@ -2,10 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 from flask_bootstrap import Bootstrap
 from flask import render_template
-
+from app import miners
 
 app = Flask(__name__)
 Bootstrap(app)
+
+rigs = [miners.MiningRig(123), miners.MiningRig(456), miners.MiningRig(789), miners.MiningRig(532), miners.MiningRig(10)]
 
 @app.route("/")
 def hello():
@@ -17,7 +19,7 @@ def network_page():
 
 @app.route("/my-rigs")
 def myrig_page():
-    return render_template('my_rigs.html')
+    return render_template('my_rigs.html', rigs= rigs)
 
 if __name__ == "__main__":
     app.run()
